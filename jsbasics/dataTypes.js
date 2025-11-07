dd79cbab17b0c6c0411fa1c17ecb4fd04f6629d7
Ashesh Gurung <ashesh.gurung@eclathealth.com>
2025-11-07 07:37:31 +0545

    feat: js basics done

---
+++ jsbasics/dataTypes.js
@@ -0,0 +1,51 @@
+animal;
+// let nameOfCollege = "Techspire";
+// const age = 2;
+// const yourAgeGraterThan21 = true;
+
+// nameOfCollege = "moktan";
+
+// const home = [
+//   "budhanilkantha",
+//   "baneshowr",
+//   "putalisadak",
+//   10,
+//   true,
+//   null,
+//   undefined,
+// ];
+
+// const myDetails = {
+//   firstName: "Ashesh",
+//   lastName: "Gurung",
+//   isMarried: false,
+//   phNum: [9800000000, 9811111111],
+//   hasPrCard: null,
+//   address: undefined,
+//   city: {
+//     temp: "ktm",
+//     per: "dharan",
+//   },
+// };
+
+// console.log(animal);
+var animal = "Dog";
+
+abc();
+
+function abc() {
+  const flower = "Rose";
+  console.log(flower, "this is consoled fro normal func");
+}
+
+const abc2 = () => {
+  const flower = "Rose";
+  console.log(flower, "this is consoled fro arrow func");
+};
+abc2();
+
+const userName = (lastName, location) => {
+  console.log(`Ashmita ${lastName} lives in ${location}`);
+};
+
+userName("lainchaur", "katuwal");
