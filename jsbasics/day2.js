2625de6174d78fb37e9c2fe0ab995df413b638c8
Ashesh Gurung <ashesh.gurung@eclathealth.com>
2025-11-11 08:42:46 +0545

    feat: hoistinf and arrow func

---
+++ jsbasics/day2.js
@@ -0,0 +1,30 @@
+// // console.log(collegename);
+
+// var collegename = "Techspire";
+// {
+//   var cr = "starling";
+// }
+// console.log(cr);
+// // collegeName();
+// function collegeName() {}
+
+// const collegeNameV2 = () => {
+//   console.log("this is arrow function");
+// };
+// // collegeNameV2();
+
+// const firstname = "Starling";
+// const lastname = "thapa";
+
+// console.log(firstname + " " + lastname);
+// console.log(`${firstname} ${lastname} is CR of A6`);
+
+function printName(firstname, lastname) {
+  console.log(`${firstname} ${lastname} is CR of A6 from printName function`);
+}
+
+const printNameV2 = (firstname, lastname) => {
+  console.log(
+    `${firstname} ${lastname} is CR of A6 from printNameV2 arrow function`
+  );
+};
